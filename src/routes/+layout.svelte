fded284c98cf9999fba6990933ca00051b07c71d
Larry Amiel Tablando <tlarryamiel@gmail.com>
2025-11-21 01:48:50 +0800

    Added metatags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,33 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	
+	<!-- Primary Meta Tags -->
 	<title>larryamiel.dev - Full Stack Web Developer</title>
+	<meta name="title" content="larryamiel.dev - Full Stack Web Developer" />
+	<meta name="description" content="I'm Larry Amiel. I'm a full stack developer based in the Golden City of Friendship of the Philippines.
+
+	I have always loved the art of problem solving, this turned me from math to computer science. Since I have started my journey, I have always been drawn to work on projects that have real world impact." />
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://larryamiel.dev" />
+	<meta property="og:title" content="larryamiel.dev - Full Stack Web Developer" />
+	<meta property="og:description" content="I'm Larry Amiel. I'm a full stack developer based in the Golden City of Friendship of the Philippines.
+
+	I have always loved the art of problem solving, this turned me from math to computer science. Since I have started my journey, I have always been drawn to work on projects that have real world impact." />
+	<meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+
+	<!-- X (Twitter) -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="https://larryamiel.dev" />
+	<meta property="twitter:title" content="larryamiel.dev - Full Stack Web Developer" />
+	<meta property="twitter:description" content="I'm Larry Amiel. I'm a full stack developer based in the Golden City of Friendship of the Philippines.
+
+	I have always loved the art of problem solving, this turned me from math to computer science. Since I have started my journey, I have always been drawn to work on projects that have real world impact." />
+	<meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+
+	<!-- Meta Tags Generated with https://metatags.io -->
 </svelte:head>
 
 {@render children()}
